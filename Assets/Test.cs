4170d6f88e0516c9a2d89daad44f863ffa0c9314
karasun2016 <sunandtoon@gmail.com>
2016-06-29 14:31:30 +0700

    edit

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -6,7 +6,7 @@ public class test : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         Debug.Log("A");
-        Debug.Log("B");
+        Debug.Log("A");
         Debug.Log("C");
     }
 	
